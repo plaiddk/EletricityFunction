83e31a039b44e3642f0f5011eba421917afb7684
Jensen <plaiddk@gmail.com>
2022-08-24 02:14:28 +0200

    Create Costview.sql

---
+++ AzureDatabaseDDL/Views/Costview.sql
@@ -0,0 +1,50 @@
+
+
+CREATE view [dm].[costview] as 
+ WITH rawdata
+AS (
+	SELECT a.*
+		,b.[name] AS PriceName
+		,b.[description] AS PriceDescription
+		,b.prices as PriceDKR		
+	FROM dm.HourDataHistory a
+	LEFT JOIN [dm].[PriceHistory] b ON a.DataStart BETWEEN b.ValidFromDate AND b.ValidToDate
+	
+	UNION ALL
+	
+	SELECT a.*
+		,'FlexEL' AS PriceName
+		,'Variable ELPris' AS PriceDescription
+		,b.PriceDKR		
+	FROM dm.HourDataHistory a
+	LEFT JOIN [dm].hourlyprices b ON a.DataStart = b.DataStartPrice
+		AND a.HourTime = b.HourTime
+	)
+SELECT DataSetStartDate
+	,DatasetEndDate
+	,Unit
+	,HourTimeStatus
+
+	,a.HourTime
+	,a.KiloWatt
+	,DAtaStart
+	,DataDateName
+	,DataEnd
+	,PriceName
+	,PriceDescription 
+	,PriceDKR
+	,case when PriceName = 'Refusion' then 0 else (PriceDKR * 1.25) *a.KiloWatt end AS CostsMoms
+	,case when PriceName = 'Refusion' then 0 else (a.KiloWatt * PriceDKR) end AS CostExMoms
+	--,ElCosts = (KiloWatt * ElPriceDKR) * 1.25
+	,b.Kilowatt as ElBilKwh
+	,case when PriceName = 'Refusion' then 1.10 * b.KiloWatt end as Refusion
+	,case when  b.Kilowatt is not null then 'ELBil' else 'Normal' end as ForbrugsType
+	
+FROM rawdata a
+
+left join dm.monta_v b on a.DAtaStart = b.StartTid and a.HourTime = b.HourTime
+cross apply ( select * from dm.PriceHistory  x where tariffs_Id = 6-- and a.DAtaStart > x.ValidFromDate
+) u
+--where DAtaStart >= '2022-06-09'
+--where b.Kilowatt is not null
+--order by DAtaStart,HourTime
