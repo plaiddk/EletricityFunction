a2391a008c9ddc66b6b7bab0982d884e95934518
Jensen <plaiddk@gmail.com>
2022-09-08 22:53:28 +0200

    Update hourlyprices.sql
    removed unused columns

--- AzureDatabaseDDL/Views/hourlyprices.sql
+++ AzureDatabaseDDL/Views/hourlyprices.sql
@@ -2,7 +2,7 @@
 CREATE view [dm].[hourlyprices] as 
 
 with dataset as (
-Select SportPriceEur, HourUTC,HourDK,HourTime,DataStartPrice,[_full_text],[_id],PriceArea,SpotPriceDKK
+Select SportPriceEur, HourUTC,HourDK,HourTime,DataStartPrice,PriceArea,SpotPriceDKK
       ,PriceDKR =     cast(replace([SpotPriceDKK],',','.') as decimal(18,2)) / 1000 
 	  ,PriceDKRMoms = cast(replace([SpotPriceDKK],',','.') as decimal(18,2)) / 1000 * 1.25
 	  from (
@@ -10,9 +10,7 @@ SELECT [SportPriceEur]
       ,[HourUTC]
       ,[HourDK]
 	  ,substring(convert(varchar(50),[HourDK],114),1,5) as HourTime
-	  ,cast(HourDK as date) as DataStartPrice
-      ,[_full_text]
-      ,[_id]
+	  ,cast(HourDK as date) as DataStartPrice  
       ,[PriceArea]
 	-- , cast(replace(SportPriceEur,',','.') as decimal(18,2))*7.4 
       ,case when cast(replace([SpotPriceDKK],',','.') as decimal(18,2)) is null then cast(replace(SportPriceEur,',','.') as decimal(18,2))*7.4 else cast(replace([SpotPriceDKK],',','.') as decimal(18,2)) end as SpotPriceDKK
