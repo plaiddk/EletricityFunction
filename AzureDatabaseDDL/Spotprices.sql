f3a97eea4ce4593eda76788b11aa03c5adcbad39
Jensen <plaiddk@gmail.com>
2022-09-08 22:53:52 +0200

    Update Spotprices.sql
    removed unused columns

--- AzureDatabaseDDL/Spotprices.sql
+++ AzureDatabaseDDL/Spotprices.sql
@@ -1,9 +1,7 @@
 CREATE TABLE [dbo].[Spotprices](
 	[SportPriceEur] [varchar](50) NULL,
 	[HourUTC] [datetime] NULL,
-	[HourDK] [datetime] NULL,
-	[_full_text] [nvarchar](max) NULL,
-	[_id] [varchar](50) NULL,
+	[HourDK] [datetime] NULL,	
 	[PriceArea] [varchar](50) NULL,
 	[SpotPriceDKK] [varchar](50) NULL
 ) ON [PRIMARY]
